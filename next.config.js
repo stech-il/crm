3c0d16aff9d03221afaeaad3eaef8332e0ff9520
freund <tvisrael@gmail.com>
2026-02-03 19:09:56 +0200

    Initial commit: CRM Cloud - Next.js, Prisma, Render

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 module.exports = nextConfig;
